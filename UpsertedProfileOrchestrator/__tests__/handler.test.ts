fd68d3c29f4bbd7b10add8a16f32677a5e65095d
Daniele Manni <dan.manni@gmail.com>
2021-06-01 15:33:57 +0200

    [#IPV-52] Fix unit test

--- UpsertedProfileOrchestrator/__tests__/handler.test.ts
+++ UpsertedProfileOrchestrator/__tests__/handler.test.ts
@@ -2,7 +2,7 @@
 
 import * as df from "durable-functions";
 import { Task } from "durable-functions/lib/src/classes";
-import { NonEmptyArray } from "fp-ts/lib/NonEmptyArray";
+import { fromArray } from "fp-ts/lib/NonEmptyArray";
 import { readableReport } from "italia-ts-commons/lib/reporters";
 import { NonEmptyString } from "italia-ts-commons/lib/strings";
 import { context as contextMock } from "../../__mocks__/durable-functions";
@@ -195,9 +195,7 @@ describe("UpsertedProfileOrchestrator", () => {
     };
 
     const orchestratorHandler = getUpsertedProfileOrchestratorHandler({
-      notifyOn: ([expectedQueueName] as unknown) as NonEmptyArray<
-        NonEmptyString
-      >,
+      notifyOn: fromArray([expectedQueueName]).getOrElseL(undefined),
       sendCashbackMessage: true
     })(contextMockWithDf as any);
 
